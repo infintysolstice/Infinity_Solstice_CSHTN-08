350a6e4d23635d3d86ab6adc560df6aaa5992ec5
Samyak Shah <s4samyak@gmail.com>
2021-09-23 17:34:19 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1>Edu Site</h1>
     </div>
   );
 }
